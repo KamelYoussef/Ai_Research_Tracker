cabf2b8c96ba82269dc944659aad7349bd26af62
youssefkamel <kamelyoussef1996@gmail.com>
2026-01-13 17:24:00 +0100

    split maps and ai_track

--- app/scripts/ai_track.py
+++ app/scripts/ai_track.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 from sqlalchemy.orm import Session
 from app.database import SessionLocal
-from app.services.storage import store_response, store_sources, store_maps
-from app.utils.helpers import track_responses, get_insurance_brokers_by_city, find_target_rank_by_city_and_keyword
+from app.services.storage import store_response, store_sources
+from app.utils.helpers import track_responses
 from app.database import Base, engine
 import time
 from collections import Counter
@@ -67,41 +67,8 @@ def daily_track(ai_platfrom):
         db.close()
 
 
-def maps_track():
-    """Function to be executed daily."""
-    current_date = datetime.now().strftime("%Y%m")
-    current_day = datetime.now().strftime("%d")
-    db: Session = SessionLocal()
-    try :
-        results = find_target_rank_by_city_and_keyword(get_insurance_brokers_by_city("app/config.yml"), "app/config.yml")
-        print(results)
-        for result in results:
-            product = result.get('product')
-            location = result.get('location')
-            rank = result.get('rank')
-            rating = result.get('rating')
-            reviews = result.get('reviews')
-
-            store_maps(
-                db=db,
-                product=product,
-                location=location,
-                date=current_date,
-                day=current_day,
-                rank=rank,
-                rating=rating,
-                reviews=reviews
-            )
-
-    except Exception as e:
-        print(f"An error occurred: {e}")
-    finally:
-        db.close()
-
-
 if __name__ == "__main__":
     startup()
-    maps_track()
     start_time = time.time()
 
     daily_track("CHATGPT")
